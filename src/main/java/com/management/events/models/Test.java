850a33d5f4e70cbcb6acdeee49ef71b1402d3881
TsinjoAH <52tsinjo@gmail.com>
2023-01-29 10:49:16 +0300

    hibernate dao integration

---
+++ src/main/java/com/management/events/models/Test.java
@@ -0,0 +1,31 @@
+package com.management.events.models;
+
+import javax.persistence.*;
+
+@Entity
+public class Test {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column
+    private String name;
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
--- src/main/webapp/views/index.jsp
+++ src/main/webapp/views/index.jsp
@@ -1,1 +1,10 @@
-<h1>HAHA</h1>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+
+<table>
+    <c:forEach var="item" items="${testList}">
+        <tr>
+            <td>${item.getId()}</td>
+            <td>${item.getName()}</td>
+        </tr>
+    </c:forEach>
+</table>
